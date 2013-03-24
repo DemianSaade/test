f5d73f5f7376d4bef6211d68c037ae904f040942
DemianSaade <d.saade@hotmail.com>
2013-03-24 06:06:42 -0300

    commit

--- src/tt.py
+++ src/tt.py
@@ -3,33 +3,45 @@ from pyglet.window import key, event
 
 
 class Gui():
-    def __init__(self):
+    def __init__(self, layout=None):
         self.show = False
+        self.layout = layout
 
-    def info(self, option, layer=None):
-        self.layout = text.Label('FPS: %f' % clock.get_fps(),
-                                 font_name='arial',
-                                 font_size=10,
-                                 color=(255, 255, 255, 255),
-                                 anchor_x='left',
-                                 anchor_y='center',
-                                 x=1, y=10)
-        if option == 't':
-            self.show = not self.show
-        if option == 'c':
-            return bool(self.show)
-        if option == 'd':
-            self.show = True
-            self.layout.draw()
+    def info(self):
+        def fps(self):
+            return 'FPS: %i' % clock.get_fps()
+
+        def toogle(self, option):
+            if hasattr(self.__class__, option):
+                self.option = not self.option
+
+        def check(self, option):
+            if hasattr(self.__class__, option):
+                return bool(self.option)
+
+        def update(self):
+            self.layout.begin_update()
+            self.layout.text = self.info.fps()
+            self.layout.end_update()
 
     def draw(self):
         if self.show:
-            self.info('d')
+            self.info.update()
+            self.layout.draw()
 
-win = window.Window(width=800, height=600)
-#win2 = window.Window(width=800, height=600)
+win = window.Window(width=800,
+                    height=600,
+                    resizable=True,
+                    caption="Test")
+
 event_loop = app.EventLoop()
 gui = Gui()
+gui.layout = text.Label(font_name='arial',
+                        font_size=20,
+                        color=(255, 255, 255, 255),
+                        anchor_x='left',
+                        anchor_y='center',
+                        x=1, y=15)
 
 clock.set_fps_limit(60)
 
@@ -38,6 +50,7 @@ clock.set_fps_limit(60)
 def on_draw():
     win.clear()
     gui.draw()
+    asd(gui.info('fps'))
     asd(str(gui.info('c')))
     asd('draw')
 
@@ -51,7 +64,7 @@ def on_key_press(symbol, modifiers):
 
 
 @event_loop.event
-def on_window_close(window):
+def on_window_close(win):
     app.exit()
     asd('exit')
 
